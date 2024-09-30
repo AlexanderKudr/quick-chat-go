056a0b8e075536c2ac29efdea13250d5e2449157
alex <trevan98@gmail.com>
2024-09-30 20:22:01 +0300

    fixes

--- client/main.go
+++ client/main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 
 	"quick-chat-go/client/pkg"
-	template "quick-chat-go/client/template"
+	"quick-chat-go/client/template"
 )
 
 const port = ":8080"
@@ -13,7 +13,7 @@ const port = ":8080"
 func main() {
 
 	wsRoom := ws.NewRoom()
-	http.Handle("/ws", &template.TemplateHandler{Filename: "index.html"})
+	http.Handle("/ws", &serve.TemplateHandler{Filename: "index.html"})
 	http.Handle("/ws-room", wsRoom)
 	go wsRoom.Run()
 
